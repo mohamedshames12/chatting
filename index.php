e6659e6af3f264e53b8fb2b86f989b03d179ae36
mohamed shames <mohamedhames900@gmail.com>
2023-03-24 22:39:47 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+    <?php
+     
+     $success_msg[] = "success message from index page!";
+    ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+    <title>chatting</title>
+</head>
+<body>
+    
+
+        <!-- sweetalert cdnjs link -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+        <!-- JS files -->
+        <script src="js/script.js"></script>
+        <?php include "php/alert.php"; ?>
+</body>
+</html>
